0785da30433e1a6623a41feeb4acf0f708a71778
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-10-14 13:38:00 -0400

    Add project files.

---
+++ Part 8 Exercises/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_8_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            practice();
+            Console.ReadLine();
+        }
+        static void practice()
+        {
+            string name = "Aldworth";
+            Console.WriteLine(name.Substring());
+
+        }
+    }
+}
